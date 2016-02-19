e601b344f2706ce9042ace836d631a07945eef2e
unknown <vlado.ivkovic@live.com>
2016-02-19 08:54:23 +0100

    final commit, presentation edited

--- code/PSP NN.py
+++ code/PSP NN.py
@@ -1,20 +1,24 @@
 import nn3psp
 import inOutFunctions
 
-datasetFile = 'rs126.fa'
-groupsFile = 'RS126/groups.txt'
-
-protDict = inOutFunctions.getProteinDict(datasetFile)
-groups = inOutFunctions.getGroups(groupsFile)
-
-
-#for sw in range(7, 16, 2):
-#    print 'sw = ' + str(sw)
-#    
-#    for x in range(7):
-sw, x = 7, 3
-for x in range(7):
-    print x
-    ann = nn3psp.make_NN(sw, protDict, groups, x)
+def main():
+    datasetFile = 'rs126.fa'
+    groupsFile = 'RS126/groups.txt'
     
-    print nn3psp.test_NN(ann, sw, protDict, groups, x)
+    protDict = inOutFunctions.getProteinDict(datasetFile)
+    groups = inOutFunctions.getGroups(groupsFile)
+    
+    
+    #for sw in range(7, 16, 2):
+    #    print 'sw = ' + str(sw)
+    #    
+    #    for x in range(7):
+    sw, x = 13, 3
+    for x in range(7):
+        print x
+        ann = nn3psp.make_NN(sw, protDict, groups, x)
+        
+        print nn3psp.test_NN(ann, sw, protDict, groups, x)
+        
+if __name__ == "__main__":
+    main()
--- code/PSP SVM.py
+++ code/PSP SVM.py
@@ -1,39 +1,40 @@
 import svm3psp
 import inOutFunctions
 
-step = 1
-
 datasetFile = 'rs126.fa'
 groupsFile = 'RS126/groups.txt'
 
 protDict = inOutFunctions.getProteinDict(datasetFile)
 groups = inOutFunctions.getGroups(groupsFile)
 
-# sliding window
-for sw in range(7, 16, 2):
-    
-    print 'sw = ' + str(sw)
+def main():  
+    # sliding window
+    for sw in range(7, 16, 2):
+        
+        print 'sw = ' + str(sw)
+        
+        for x in range(7):
+            clf = svm3psp.make_SVM_3X(sw, protDict, groups, x)
+            
+            print svm3psp.test_SVM_3X(clf, sw, protDict, groups, x)
+
+def test(trained):
+    sw = 15
+    x = 2
     
     filename = 'dump.pkl'
-    trained = False
     
-    for x in range(7):
-#        if trained:
-#            clf = svm3psp.loadSVM(filename)
-#        else:
+    if trained:
+        clf = svm3psp.loadSVM(filename)
+    else:
         clf = svm3psp.make_SVM_3X(sw, protDict, groups, x)
-            #svm3psp.saveSVM(clf, filename)
+        svm3psp.saveSVM(clf, filename)
+            
+    print svm3psp.test_SVM_3X(clf, sw, protDict, groups, x)
         
-    #    q3 = 0
-    #    while x < 110:
-    #        rslt = svm3psp.test_SVM_3(clf, step, x, sw, dataset)
-    #        x += step
-    #        
-    #        #print rslt
-    #    
-    #        q3 += rslt[0]
-    #    
-    #    print q3/30
-        print svm3psp.test_SVM_3X(clf, sw, protDict, groups, x)
+    
+
+if __name__ == "__main__":
+    test(True)
 
 
